f3030f2e58164e87c5a72bb57519c27210436cf6
Masthead <alexeev.tolik2015@yandex.ru>
2023-03-11 16:44:40 +0300

    comments-blog

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     devtool: "source-map",
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
         filename: 'comments.js'
     },
